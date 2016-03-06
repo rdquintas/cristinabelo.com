f6fa280325beedaded7ba6f058ea3d6a8f471e2c
rdquintas <rqcenter-web@yahoo.com>
2016-03-06 17:10:49 +0000

    adding STANDARDJS to the dev workflow process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,109 +1,113 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  var js_files_vendor = [
+    'user/themes/zrq/js/vendor/jquery.parallax-1.1.3.js',
+    'user/themes/zrq/js/vendor/jquery.localscroll-1.2.7-min.js',
+    'user/themes/zrq/js/vendor/jquery.scrollTo-1.4.2-min.js'
+  ]
 
-    var js_files_vendor = [
-        'user/themes/zrq/js/vendor/jquery.parallax-1.1.3.js',
-        'user/themes/zrq/js/vendor/jquery.localscroll-1.2.7-min.js',
-        'user/themes/zrq/js/vendor/jquery.scrollTo-1.4.2-min.js'
-    ];
+  var js_files = [
+    'user/themes/zrq/js/app.js'
+  ]
 
-    var js_files = [
-        'user/themes/zrq/js/app.js'
-    ];
+  var css_files = [
+    'user/themes/zrq/css/globals.css',
+    'user/themes/zrq/css/home_page.css',
+    'user/themes/zrq/css/default_page.css',
+    'user/themes/zrq/css/bio_page.css'
+  ]
 
-    var css_files = [
-        'user/themes/zrq/css/globals.css',
-        'user/themes/zrq/css/home_page.css',
-        'user/themes/zrq/css/default_page.css',
-        'user/themes/zrq/css/bio_page.css'
-    ];
+  // grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-standard')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
+  // ================================
+  // Task:    default
+  // ================================
+  grunt.registerTask('default', [
+    'standard:js_files',
+    'concat:js_files',
+    'concat:js_files_vendor',
+    'concat:css_files'
+  ])
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     // ================================
-    // Task:    default
+    // WATCH
     // ================================
-    grunt.registerTask('default', [
-        'jshint:js_files',
-        'concat:js_files',
-        'concat:js_files_vendor',
-        'concat:css_files',
-    ]);
-
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        // ================================
-        // WATCH
-        // ================================          
-        watch: {
-            configFiles: {
-                files: ['Gruntfile.js'],
-                options: {
-                    reload: true
-                },
-                tasks: [
-                    'default'
-                ],
-            },
-            js_files: {
-                files: js_files,
-                tasks: [
-                    'jshint:js_files',
-                    'concat:js_files',
-                    'concat:js_files_vendor'
-                ],
-                options: {
-                    event: ['all'],
-                    interrupt: true
-                }
-            },
-            css_custom: {
-                files: "user/themes/zrq/css/*.css",
-                tasks: [
-                    'concat:css_files'
-                ],
-                options: {
-                    event: ['all'],
-                    interrupt: true
-                }
-            }
+    watch: {
+      configFiles: {
+        files: ['Gruntfile.js'],
+        options: {
+          reload: true
         },
-
-
-        // ================================
-        // JSHINT
-        // ================================          
-        jshint: {
-            js_files: {
-                src: js_files
-            }
-        },
-
-
-        // ================================
-        // CONCAT
-        // ================================          
-        concat: {
-            options: {
-                sourceMap: true
-            },
-            js_files_vendor: {
-                src: js_files_vendor,
-                dest: 'user/themes/zrq/js/dist/libs.dist.js'
-            },
-            js_files: {
-                src: js_files,
-                dest: 'user/themes/zrq/js/dist/app.dist.js'
-            },
-            css_files: {
-                src: css_files,
-                dest: 'user/themes/zrq/css/dist/app.dist.css'
-            }
+        tasks: [
+          'default'
+        ]
+      },
+      js_files: {
+        files: js_files,
+        tasks: [
+          'standard:js_files',
+          'concat:js_files',
+          'concat:js_files_vendor'
+        ],
+        options: {
+          event: ['all'],
+          interrupt: true
         }
+      },
+      css_custom: {
+        files: 'user/themes/zrq/css/*.css',
+        tasks: [
+          'concat:css_files'
+        ],
+        options: {
+          event: ['all'],
+          interrupt: true
+        }
+      }
+    },
 
-    });
-};
+    // // ================================
+    // // STANDARDJS
+    // // ================================
+    standard: {
+      js_files: {
+        src: js_files
+      }
+    },
+
+    // // ================================
+    // // JSHINT
+    // // ================================
+    // jshint: {
+    //   js_files: {
+    //     src: js_files
+    //   }
+    // },
+
+    // ================================
+    // CONCAT
+    // ================================
+    concat: {
+      options: {
+        sourceMap: true
+      },
+      js_files_vendor: {
+        src: js_files_vendor,
+        dest: 'user/themes/zrq/js/dist/libs.dist.js'
+      },
+      js_files: {
+        src: js_files,
+        dest: 'user/themes/zrq/js/dist/app.dist.js'
+      },
+      css_files: {
+        src: css_files,
+        dest: 'user/themes/zrq/css/dist/app.dist.css'
+      }
+    }
+
+  })
+}
--- user/themes/zrq/js/app.js
+++ user/themes/zrq/js/app.js
@@ -1,52 +1,52 @@
-$(document).ready(function() {
+var $ = window.$
+var imagesArr
 
-    // Initialize Parallax on Home Page
-    if ($('section#home').length > 0) {
-        var speed = 0;
+$(document).ready(function () {
+  // Initialize Parallax on Home Page
+  if ($('section#home').length > 0) {
+    var speed = 0
 
-        for (var i = 0; i < imagesArr.length; i++) {
-            if (i < 10) {
-                // switch (i) {             
-                //     case 1:
-                //         speed = 0.6;
-                //         break;
-                //     case 2:
-                //         speed = 0.6;
-                //         break;
-                //     default:
-                //         speed = 0.6;
-                //         break;
-                // }
-                speed = 0.6;
-                $('#parallax' + (i + 1)).css("background", "url(" + imagesArr[i] + ") 50% 0 no-repeat fixed");
+    for (var i = 0; i < imagesArr.length; i++) {
+      if (i < 10 && imagesArr[i] !== null) {
+        // switch (i) {
+        //     case 1:
+        //         speed = 0.6
+        //         break
+        //     case 2:
+        //         speed = 0.6
+        //         break
+        //     default:
+        //         speed = 0.6
+        //         break
+        // }
+        speed = 0.6
+        $('#parallax' + (i + 1)).css('background', 'url(' + imagesArr[i] + ') 50% 0 no-repeat fixed')
 
-                if ($(window).width() > 900) {
-                    $('#parallax' + (i + 1)).parallax("50%", speed);
-                }
-
-            }
+        if ($(window).width() > 900) {
+          $('#parallax' + (i + 1)).parallax('50%', speed)
         }
-        $('body').addClass("home-page");
-    } else {
-        $('body').removeClass("home-page");
+      }
     }
+    $('body').addClass('home-page')
+  } else {
+    $('body').removeClass('home-page')
+  }
 
-    // initialize video formatting
-    if ($('section#default-page').length > 0) {
-        $("video").attr("poster", "user/themes/zrq/images/transparent.png");
+  // initialize video formatting
+  if ($('section#default-page').length > 0) {
+    $('video').attr('poster', 'user/themes/zrq/images/transparent.png')
+  }
+
+  // copy the language switcher into the slide menu
+  $('#header ul.langswitcher li').clone().prependTo('.sb-slidebar ul.navigation')
+
+  // apply the CSS colors to the nav menu (desktop only)
+  var colorNumber = 0
+  $.each($('#header ul.navigation li'), function (index, value) {
+    colorNumber += 1
+    $(value).addClass('color' + colorNumber)
+    if (colorNumber >= 4) {
+      colorNumber = 0
     }
-
-    // copy the language switcher into the slide menu
-    $("#header ul.langswitcher li").clone().prependTo(".sb-slidebar ul.navigation");
-
-    // apply the CSS colors to the nav menu (desktop only)
-    var colorNumber = 0;
-    $.each($("#header ul.navigation li"), function(index, value) {
-        colorNumber += 1;
-        $(value).addClass("color" + colorNumber);
-        if (colorNumber >= 4) {
-            colorNumber = 0;
-        }
-    });
-
-});
+  })
+})
